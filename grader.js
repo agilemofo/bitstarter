6d140e5e900cd5192b5ef5047d73ebe60c398b8e
Woody <bglier@gmail.com>
2013-07-12 15:28:27 +0000

    finally cracked async and output

--- grader.js
+++ grader.js
@@ -26,7 +26,7 @@ var program = require('commander');
 var cheerio = require ('cheerio');
 var HTMLFILE_DEFAULT = 'index.html';
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://http://intense-bastion-4163.herokuapp.com/"
+var URL_DEFAULT = "http://intense-bastion-4163.herokuapp.com/"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -57,17 +57,16 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var geturl = function(url, checksfile) {
-    rest.get(url).on('complete', function(result){
-	$ = cheerio.load(result);
-	var checks = loadChecks(checksfile).sort();
-	var out = {};
-	for (var ii in checks) {
-	    var present = $(checks[ii]).length > 0;
-	    out [checks[ii]] = present;
-	    }
-	return out;
-};
+var checkurl = function(url, checksfile) {
+    restler.get(url).on('complete', function(result) {
+		var checkJson = checkHtmlFile(result, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+		});
+    };
+
+		   
+
 
 
 var clone = function(fn) {
@@ -80,20 +79,13 @@ if(require.main == module) {
     program
     .option ('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-    .option ('-u, --url <url>', 'URL', String, URL_DEFAULT) 
+    .option ('-u, --url <url>', 'URL', URL_DEFAULT) 
     .parse(process.argv);
-    if (program.file) {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-	}
-    if (program.url) {
-	var checkJson = geturl(program.url, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson):
-	}
-
-        } else {
+}
+       else {
 	exports.checkHtmlFile = checkHtmlFile;
 }
 
