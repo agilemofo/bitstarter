4d818a83d928c8e3b4a642bd2757f93267572d41
Woody <bglier@gmail.com>
2013-07-12 07:27:59 +0000

    Still working on troubleshooting the geturl function

--- grader.js
+++ grader.js
@@ -21,10 +21,12 @@ Referenes:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require ('cheerio');
 var HTMLFILE_DEFAULT = 'index.html';
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://http://intense-bastion-4163.herokuapp.com/"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +57,19 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var geturl = function(url, checksfile) {
+    rest.get(url).on('complete', function(result){
+	$ = cheerio.load(result);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for (var ii in checks) {
+	    var present = $(checks[ii]).length > 0;
+	    out [checks[ii]] = present;
+	    }
+	return out;
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,11 +80,20 @@ if(require.main == module) {
     program
     .option ('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option ('-u, --url <url>', 'URL', String, URL_DEFAULT) 
     .parse(process.argv);
+    if (program.file) {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-    } else {
+	}
+    if (program.url) {
+	var checkJson = geturl(program.url, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson):
+	}
+
+        } else {
 	exports.checkHtmlFile = checkHtmlFile;
 }
 
